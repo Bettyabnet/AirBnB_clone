0ae3b2dba5eb2638eb66c96584183b7157ecec32
root <root@BettysHp>
2023-11-09 14:27:36 +0300

    console works

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""The program which containes the entry point of the command interpreter"""
+
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit on EOF (Ctrl+D)"""
+        return True
+
+    def do_help(self, arg):
+        """List available commands with "help" or detailed help with "help cmd"."""
+        cmd.Cmd.do_help(self, arg)
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered."""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
