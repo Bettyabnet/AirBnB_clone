eb7cc5dbf74634ecd7cb3958e9b0e9522ef81b11
root <root@BettysHp>
2023-11-12 14:59:56 +0300

    self

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python3
+import unittest
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+class TestBaseModel(unittest.TestCase):
+
+    def test_str_method(self):
+        """Test the __str__() method of the BaseModel class"""
+        model = BaseModel()
+        expected_str = f"[BaseModel] ({model.id}) {{'id': '{model.id}', 'created_at': {model.created_at}, 'updated_at': {model.updated_at}}}"
+        self.assertEqual(str(model), expected_str)
+
+    def test_save_method(self):
+        """Test the save() method of the BaseModel class"""
+        model = BaseModel()
+        model.save()
+        self.assertIsNotNone(model.updated_at)
+        self.assertIsNotNone(model.created_at)
+
+    def test_to_dict_method(self):
+        """Test the to_dict() method of the BaseModel class"""
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertIsInstance(model_dict, dict)
+        self.assertEqual(model_dict['__class__'], 'BaseModel')
+        self.assertEqual(model_dict['id'], model.id)
+        self.assertEqual(model_dict['created_at'], model.created_at.isoformat())
+        self.assertEqual(model_dict['updated_at'], model.updated_at.isoformat())
+
+if __name__ == '__main__':
+    unittest.main()
