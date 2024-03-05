f8561ae7ad15d3ba8188e032393ea75e1d0de09d
bettyabnet <betelhemabnet1@gmail.com>
2024-03-05 17:23:32 +0300

    modals

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+aminity
+"""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name = ""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,14 +3,42 @@
 update
 """
 import json
-from models.place import Place
+
 from models.state import State
+from models.place import Place
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
 class FileStorage:
-    # Existing code ...
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        serialized_objects = {}
+        for key, obj in FileStorage.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump(serialized_objects, file)
+
+    def reload(self):
+        try:
+            with open(FileStorage.__file_path, 'r') as file:
+                serialized_objects = json.load(file)
+                for key, obj_dict in serialized_objects.items():
+                    class_name = obj_dict['__class__']
+                    obj = globals()[class_name](**obj_dict)
+                    FileStorage.__objects[key] = obj
+        except FileNotFoundError:
+            pass
     
     def serialize(self, obj):
         """Serialize an object to JSON format."""
